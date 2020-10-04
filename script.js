0490e02a1cd7eea8b3c8bc33335c3bccd3cdc9c6
Uri Goryachev <ura2rist@gmail.com>
2020-10-05 00:22:29 +0300

    игра

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let leftOff = 0,
-	topOff = 0;
+	topOff = 0,
+	amount = 0,
+	speed = 200,
+	intId;
 
 function move(){
 	if(leftOff < 200 && topOff <= 0){
@@ -21,4 +24,18 @@ function move(){
 	}
 }
 
-setInterval(move, 10);
+intId = setInterval(move, speed);
+
+$('#one').click(function(event){
+	if(amount === 10){
+		clearInterval(intId);
+		$('#one').text('Вы выиграли');
+		return;
+	}else{
+		speed -= 25;
+		amount++;
+		clearInterval(intId);
+		intId = setInterval(move, speed);
+		$('#one').text(amount);
+	}
+});
